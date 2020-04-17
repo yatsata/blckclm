1f12a36a2e6252946a72f9079a72ba6ca80e79af
yavor <yavor.rosenov@gmail.com>
2020-04-17 11:10:53 +0300

    Separate the code to utility classes

--- service-a/src/main/java/com/claim/blockchain/blckclm/utils/ContractUtils.java
+++ service-a/src/main/java/com/claim/blockchain/blckclm/utils/ContractUtils.java
@@ -1,4 +1,49 @@
 package com.claim.blockchain.blckclm.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class ContractUtils {
+
+    public static final String CONTRACT_EXECUTION_RESULT = "CONTRACT_EXECUTION_RESULT";
+
+    public static String getContractResponse(){
+        String bashExecutable = "C:\\Program Files\\Git\\git-bash.exe";
+        String blochChainConfigFolder = "d:/blokchain/helloworld";
+        String shAbsPath = "";
+        String outFileAbsPath = "";
+        String jsAbsPath = "";
+        String contractName = "HelloWorld";
+        String contractAddress = "0x5DD5e5CFf778058fd45D848d36F02d1b8560d7D7";
+        String tempFolder = "D:\\blokchain_temp\\temp";
+        java.nio.file.Path tempPath = Paths.get(tempFolder);
+        try {
+            java.nio.file.Path shFile = Files.createTempFile(tempPath,"sh", ".sh");
+            shAbsPath = shFile.toFile().getAbsolutePath();
+            java.nio.file.Path outFile = Files.createTempFile(tempPath,"out", ".properties");
+            java.nio.file.Path jsFile = Files.createTempFile(tempPath,"js", ".js");
+            outFileAbsPath = outFile.toFile().getAbsolutePath();
+            jsAbsPath = jsFile.toFile().getAbsolutePath();
+            FileProcessingUtils.createJSFile(jsAbsPath, contractName, contractAddress, CONTRACT_EXECUTION_RESULT);
+            FileProcessingUtils.createSHFile(shAbsPath, jsAbsPath, outFileAbsPath, blochChainConfigFolder);
+            Process process = Runtime.getRuntime().exec(bashExecutable+ " " + shAbsPath);
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        String result = "";
+        try (InputStream input = new FileInputStream(outFileAbsPath)){
+            Properties contractResult = new Properties();
+            contractResult.load(input);
+            result = contractResult.getProperty(CONTRACT_EXECUTION_RESULT);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        FileProcessingUtils.deleteFiles(shAbsPath, outFileAbsPath, jsAbsPath);
+        return result;
+    }
 }
--- service-a/src/main/java/com/claim/blockchain/blckclm/utils/FileProcessingUtils.java
+++ service-a/src/main/java/com/claim/blockchain/blckclm/utils/FileProcessingUtils.java
@@ -1,4 +1,54 @@
 package com.claim.blockchain.blckclm.utils;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class FileProcessingUtils {
+
+    public static void createSHFile(String shFile, String jsFile, String outputFile, String blochChainConfigFolder){
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(shFile, true);
+            writer.write("cd " + blochChainConfigFolder +"\n");
+            jsFile = jsFile.replaceAll("\\\\","/");
+            outputFile = outputFile.replaceAll("\\\\","/");
+            writer.write("truffle exec " + jsFile +" | tee " + outputFile);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createJSFile(String jsFile, String contractName, String contractAddress, String result){
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(jsFile, true);
+            writer.write("var myContract = artifacts.require('" + contractName + "');\n");
+            writer.write("var contract_address = '" + contractAddress + "';\n");
+            writer.write("module.exports = function() {\n");
+            writer.write("  async function getContractResponse() {\n");
+            writer.write("    let ins = await myContract.at(contract_address);\n");
+            writer.write("    let res = await ins.getMessage();\n");
+            writer.write("    console.log(\"" + result + ":\" + res.toString())\n");
+            writer.write("  }\n");
+            writer.write("  getContractResponse();\n");
+            writer.write("}\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteFiles(String shAbsPath, String outFileAbsPath, String jsAbsPath) {
+        try {
+            Files.deleteIfExists(Paths.get(shAbsPath));
+            Files.deleteIfExists(Paths.get(outFileAbsPath));
+            Files.deleteIfExists(Paths.get(jsAbsPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
