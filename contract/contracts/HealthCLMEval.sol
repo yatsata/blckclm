2868345ae4728f0064cdfc6324bdfc7080ae88c2
yavor <yavor.rosenov@gmail.com>
2020-05-02 11:49:33 +0300

    Extended contract with simple statements

--- contract/contracts/HealthCLMEval.sol
+++ contract/contracts/HealthCLMEval.sol
@@ -2,9 +2,7 @@ pragma solidity >= 0.5.0 < 0.7.0;
 pragma experimental ABIEncoderV2;
 
 contract HealthCLMEval {
-  constructor() public {
-  }
-   struct HealthRq{
+    struct HealthRq{
         string medicalResearcherId;
         string insurerId;
         string insuredId;
@@ -16,9 +14,8 @@ contract HealthCLMEval {
         string examinationSummary;
     }
 
-    function evaluate(string memory medicalResearcherId, string memory insurerId, string memory insuredId,
-        string memory insuredBankAccount, string memory examinationId, string memory examinationTopic,
-        string memory examinationSubTopic, string memory examinationPrice,
+    function evaluate(string memory medicalResearcherId,string memory insurerId,  string memory insuredId, string memory insuredBankAccount,
+        string memory examinationId, string memory examinationTopic, string memory examinationSubTopic, string memory examinationPrice,
         string memory examinationSummary)public pure returns(string memory){
         HealthRq memory rq;
         rq.medicalResearcherId = medicalResearcherId;
@@ -34,6 +31,25 @@ contract HealthCLMEval {
     }
 
     function process(HealthRq memory recordData) public pure returns(string memory) {
-        return recordData.insuredId;
+        string memory researcherId = "10000001";
+        string memory insuredId = "20000001";
+        string memory examinationTopic = "HLT_TST";
+        string memory examinationSubTopic = "SEX_DIS";
+        string memory examinationSummaryPositive = "TRUE";
+        if(keccak256(abi.encodePacked(recordData.medicalResearcherId)) != keccak256(abi.encodePacked(researcherId))){
+            return "medicalResearcherId no in trusted medialc reseach id list";
+        }
+        if(keccak256(abi.encodePacked(recordData.insuredId)) != keccak256(abi.encodePacked(insuredId))){
+            return "Insured not client of the company";
+        }
+        if(keccak256(abi.encodePacked(recordData.examinationTopic)) != keccak256(abi.encodePacked(examinationTopic))){
+            return "Examination is not a topic of this contrat";
+        }
+        if(keccak256(abi.encodePacked(recordData.examinationSubTopic)) == keccak256(abi.encodePacked(examinationSubTopic)) &&
+        keccak256(abi.encodePacked(recordData.examinationSummary)) == keccak256(abi.encodePacked(examinationSummaryPositive))){
+            return "No payment for positive result on positive results for SEX_DIS";
+        }
+        string memory response = "Claim_Evaluation=FOR_PAYMENT\nInsured_bank_account=";
+        return string(abi.encodePacked(response, recordData.insuredBankAccount));
     }
 }
