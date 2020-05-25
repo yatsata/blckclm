f3e1d476a4a17a05b511de442839d994c4cc75fb
yavor <yavor.rosenov@gmail.com>
2020-05-25 08:59:01 +0300

    Add callerId and contractId as query params

--- service-a/src/main/java/com/claim/blockchain/blckclm/ContractClaimController.java
+++ service-a/src/main/java/com/claim/blockchain/blckclm/ContractClaimController.java
@@ -14,16 +14,14 @@ import java.util.Map;
 @Path("/contract-result")
 @Singleton
 public class ContractClaimController {
-
-    private static final String testContract = "HealthCLMEval";
-    private static final String testContractAddress = "0x8b19202841E6C3ebf56c805634FaBcA04823F796";
     private static final String separator = ", ";
 
     @POST
     @Path("/claim")
     @Consumes("application/json")
     @Produces("application/json")
-    public Map getClaimResult(HealthClaimRq request, @QueryParam(value="callerId") String callerId) {
+    public Map getClaimResult(HealthClaimRq request, @QueryParam(value="callerId") String callerId,
+                              @QueryParam(value="contractId") String contractId) {
         ContractUtils.processRequestData(request);
         Map map = new HashMap<>();
         String contractArguments = request.getMedicalResearcherId() + separator + request.getInsurerId() +
@@ -31,16 +29,17 @@ public class ContractClaimController {
                                    request.getExaminationId() + separator + request.getExaminationTopic() +
                                    request.getExaminationSubTopic() + separator + request.getExaminationPrice() +
                                    request.getExaminationSummary();
-        map.put(ContractUtils.CONTRACT_EXECUTION_RESULT, ContractUtils.execContract(testContract, testContractAddress,
+        map.put(ContractUtils.CONTRACT_EXECUTION_RESULT, ContractUtils.execContract(contractId, callerId,
                                                                                     callerId, contractArguments));
         return map;
     }
 
     public static void main(String[] args) {
         System.out.println(ContractUtils.CONTRACT_EXECUTION_RESULT + ":" +
-                           ContractUtils.execContract(testContract, testContractAddress,
-                                   "0xa70A7F1D913E6d039e33CfD99f7be6007a75b5Fd",
-                                   "10000001, 2, 3, 4, 5, 6, 7, 8, 9"));
+                           ContractUtils.execContract("HealthCLMEval",
+                                       "0x8b19202841E6C3ebf56c805634FaBcA04823F796",
+                                              "0xa70A7F1D913E6d039e33CfD99f7be6007a75b5Fd",
+                                    "10000002, 2, 3, 4, 5, 6, 7, 8, 9"));
     }
 
 }
